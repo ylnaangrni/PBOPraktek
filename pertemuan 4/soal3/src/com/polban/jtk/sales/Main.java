07d522eaa4234fb1fca18429b4ea5c35a194e359
ylnaangrni <158983457+ylnaangrni@users.noreply.github.com>
2024-09-13 18:06:50 +0700

    Add files via upload

---
+++ pertemuan 4/soal3/src/com/polban/jtk/sales/Main.java
@@ -0,0 +1,18 @@
+import com.polban.jtk.sales.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Product laptop = new Product("Laptop", 15000000, 10);
+
+        Sales sales = new Sales(laptop);
+
+        sales.sellProduct(5);
+
+        sales.restockProduct(12);
+
+        sales.updateProductPrice(14000000);
+
+        sales.updateProductPrice(-1000);
+    }
+}
